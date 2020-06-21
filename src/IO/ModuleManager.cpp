8d63c03da083d4396c9dc3edeb00242813a3f5b9
VELBN <8317833+velbn@users.noreply.github.com>
2020-06-21 20:07:17 +0200

    add demo functionality to Modulemanager

--- src/IO/ModuleManager.cpp
+++ src/IO/ModuleManager.cpp
@@ -9,89 +9,121 @@ ModuleManager::ModuleManager()
 void ModuleManager::DetectModules()
 {
 
-    _baseModule = new VM208();
-    _modules.push_back(_baseModule);
-    _baseModule->initialize();
-    _baseModule->turnAllChannelsOff();
-    uint _channelIndex = 0;
-    for (int i = 0; i < 4; i++)
+    if (_demo)
     {
-        _baseModule->getChannel(i)->setName(config.getNameFromChannel(_channelIndex));
-        Serial.println(config.getNameFromChannel(_channelIndex));
-        _channelIndex++;
-    }
-    for (byte i = 0x70; i < 0x78; i++)
-    {
-        Wire.beginTransmission(i);
-        Wire.write(0x00);
-        Wire.endTransmission();
-    }
-    Wire.beginTransmission(0x23);
-    byte error = Wire.endTransmission();
-    Serial.print("return value I2C: ");
-    Serial.println(error);
-    if (!error)
-    {
-        Serial.println("native VM208EX found");
-        _extensionModule = new VM208EX();
-        _modules.push_back(_extensionModule);
-        _extensionModule->initialize();
-        _extensionModule->turnAllChannelsOff();
-        for (int i = 0; i < 8; i++)
+        uint _channelIndex = 0;
+        _baseModule = new VM208();
+        _modules.push_back(_baseModule);
+        for (int i = 0; i < 4; i++)
         {
-            _extensionModule->getChannel(i)->setName(config.getNameFromChannel(_channelIndex));
+            _baseModule->getChannel(i)->setName(config.getNameFromChannel(_channelIndex));
+            Serial.println(config.getNameFromChannel(_channelIndex));
             _channelIndex++;
+        }
+        for (byte address = 0x70; address < 0x78; ++address)
+        {
+
+            _interfaces[address - 0x70].setAddress(address);
+            for (byte socket = 4; socket < 8; socket++)
+            {
+                VM208EX *module = new VM208EX(_interfaces[address - 0x70].getSocket(socket - 3));
+                _modulesOnInterface[address - 0x70].push_back(module);
+                auto interface = _modulesOnInterface[address - 0x70];
+                interface[interface.size() - 1]->initialize();
+                for (int i = 0; i < 8; i++)
+                {
+                    module->getChannel(i)->setName(config.getNameFromChannel(_channelIndex));
+                    _channelIndex++;
+                }
+            }
         }
     }
     else
     {
-        _channelIndex += 8;
-    }
-    for (byte address = 0x70; address < 0x78; ++address)
-    {
-        Wire.beginTransmission(address);
-        error = Wire.endTransmission();
-
-        Serial.print("address: ");
-        Serial.println(address, HEX);
-        Serial.print("return value I2C ");
+        _baseModule = new VM208();
+        _modules.push_back(_baseModule);
+        _baseModule->initialize();
+        _baseModule->turnAllChannelsOff();
+        uint _channelIndex = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            _baseModule->getChannel(i)->setName(config.getNameFromChannel(_channelIndex));
+            Serial.println(config.getNameFromChannel(_channelIndex));
+            _channelIndex++;
+        }
+        for (byte i = 0x70; i < 0x78; i++)
+        {
+            Wire.beginTransmission(i);
+            Wire.write(0x00);
+            Wire.endTransmission();
+        }
+        Wire.beginTransmission(0x23);
+        byte error = Wire.endTransmission();
+        Serial.print("return value I2C: ");
         Serial.println(error);
         if (!error)
         {
-            _interfaces[address - 0x70].setAddress(address);
-            for (byte socket = 4; socket < 8; socket++)
+            Serial.println("native VM208EX found");
+            _extensionModule = new VM208EX();
+            _modules.push_back(_extensionModule);
+            _extensionModule->initialize();
+            _extensionModule->turnAllChannelsOff();
+            for (int i = 0; i < 8; i++)
             {
-                Wire.beginTransmission(address);
-                Wire.write(socket);
-                Wire.endTransmission();
-
-                Wire.beginTransmission(0x23);
-                error = Wire.endTransmission();
-                if (!error)
-                {
-                    Serial.print("VM208EX found on interface address: ");
-                    Serial.println(address, HEX);
-                    Serial.print("socket: ");
-                    Serial.println(socket - 3);
-                    VM208EX *module = new VM208EX(_interfaces[address - 0x70].getSocket(socket - 3));
-                    _modulesOnInterface[address - 0x70].push_back(module);
-                    auto interface = _modulesOnInterface[address - 0x70];
-                    interface[interface.size() - 1]->initialize();
-                    for (int i = 0; i < 8; i++)
-                    {
-                        module->getChannel(i)->setName(config.getNameFromChannel(_channelIndex));
-                        _channelIndex++;
-                    }
-                }
-                else
-                {
-                    _channelIndex += 8; //we skipped a module
-                }
+                _extensionModule->getChannel(i)->setName(config.getNameFromChannel(_channelIndex));
+                _channelIndex++;
             }
         }
         else
         {
-            _channelIndex += 32; //we skipped 32 relays (4 modules with 8 channels)
+            _channelIndex += 8;
+        }
+        for (byte address = 0x70; address < 0x78; ++address)
+        {
+            Wire.beginTransmission(address);
+            error = Wire.endTransmission();
+
+            Serial.print("address: ");
+            Serial.println(address, HEX);
+            Serial.print("return value I2C ");
+            Serial.println(error);
+            if (!error)
+            {
+                _interfaces[address - 0x70].setAddress(address);
+                for (byte socket = 4; socket < 8; socket++)
+                {
+                    Wire.beginTransmission(address);
+                    Wire.write(socket);
+                    Wire.endTransmission();
+
+                    Wire.beginTransmission(0x23);
+                    error = Wire.endTransmission();
+                    if (!error)
+                    {
+                        Serial.print("VM208EX found on interface address: ");
+                        Serial.println(address, HEX);
+                        Serial.print("socket: ");
+                        Serial.println(socket - 3);
+                        VM208EX *module = new VM208EX(_interfaces[address - 0x70].getSocket(socket - 3));
+                        _modulesOnInterface[address - 0x70].push_back(module);
+                        auto interface = _modulesOnInterface[address - 0x70];
+                        interface[interface.size() - 1]->initialize();
+                        for (int i = 0; i < 8; i++)
+                        {
+                            module->getChannel(i)->setName(config.getNameFromChannel(_channelIndex));
+                            _channelIndex++;
+                        }
+                    }
+                    else
+                    {
+                        _channelIndex += 8; //we skipped a module
+                    }
+                }
+            }
+            else
+            {
+                _channelIndex += 32; //we skipped 32 relays (4 modules with 8 channels)
+            }
         }
     }
 }
@@ -113,7 +145,9 @@ RelayModule *ModuleManager::getModule(int index)
             checkedInterfaces++;
         }
         return _modulesOnInterface[checkedInterfaces][index - previousAmountChecked];
-    }else{
+    }
+    else
+    {
         return _baseModule;
     }
 }
